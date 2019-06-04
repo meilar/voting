e047d83f1c0f739c7b3ef3c195076082785060da
Katya Lobanova and Matt Eilar <matt@>
2019-06-03 20:46:39 -0700

    create new version of political page with two question form

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
-  if (age <= 17) {
-    $('#underage').show();
-  } else {
-      $('#voter').show();
+  $("#quiz-form").submit(function(event) {
+    var okay = $("select#okay-check").val();
+
+    var year = parseInt($("input#year").val());
+
+    if (okay-check === 'okay' && year < 2019) {
+      $("#ok-past").show()
     }
+    else if (okay-check === 'okay' && year === 2019) {
+      $("#ok-present").show()
+    }
+    else if (okay-check === 'okay' && year > 2019) {
+      $("#ok-future").show()
+    }
+    else if (okay-check === 'not-okay' && year > 2019) {
+      $("#nok-future").show()
+    }
+    else if (okay-check === 'not-okay' && year === 2019) {
+      $("#nok-present").show()
+    }
+    else if (okay-check === 'not-okay' && year < 2019) {
+      $("#nok-past").show()
+    }
+    event.preventDefault();
+  });
 });
